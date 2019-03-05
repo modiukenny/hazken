eae2cda30310bc6557c7b2cf44e03f6b46d3ee70
unknown <modiukehinde@gmail.com>
2019-03-05 09:24:55 +0100

    changes

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
-    <title>::HAZKEN DIGITAL::</title>
+    <title>::HAZKEN GRAPHICS::</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">

066fb3e71bda78d54e7259cbd301a2639807d192
unknown <modiukehinde@gmail.com>
2019-03-25 23:28:23 +0100

    titleimg

--- index.php
+++ index.php
@@ -700,7 +700,8 @@
         <div class="container bg-light ">
                 <div class="media border p-3 d-flex flex-row border border-5"
                 style="background: #DCDDDF; border-radius: 25px">
-                  <img src="./src/img/hazkenceo.png" alt="John Doe" class="mr-3 mt-4 align-self-lg-start" style="width:95px; height: 120px">
+                  <img src="./src/img/hazkenceo.png" title="Modiu Kehinde Hazzan" alt="John Doe"
+                  class="mr-3 mt-4 align-self-lg-start" style="width:95px; height: 120px">
                   <div class="media-body mt-4 align-self-center">
                     <h4 class="font-weight-bolder" title="ABOUT HAZKEN GRAPHICS">ABOUT THE AUTHOR</h4>
                     <p style="font-size: 15px"><span class="font-weigth-bolder" title="Hazken Graphics CEO">

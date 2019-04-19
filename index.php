23f223341f23b9a0945080cf05d6baa414cc93fb
unknown <modiukehinde@gmail.com>
2019-04-19 21:27:48 +0100

    change

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
         </div>
     </div>
     <!--Image Slides-->
-    <div id="demo" class="carousel slide " data-ride="carousel" style="margin-top: 55px">
+    <div id="demo" class="carousel slide mt-5" data-ride="carousel">
         <ul class="carousel-indicators">
             <li data-target="#demo" data-slide-to="0" class="active"></li>
             <li data-target="#demo" data-slide-to="1"></li>

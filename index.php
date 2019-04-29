1543b2580072a0d805a4f03eb696c0d1c49601ae
unknown <modiukehinde@gmail.com>
2019-04-29 10:47:10 +0100

    scripts

--- index.php
+++ index.php
@@ -917,7 +917,16 @@
 <script src="main.js"></script>
 
 <!--Global JS-->
-<script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/popper.js/umd/popper.min.js"></script>
+	<script src="assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+	<!--Vendors-->
+	<script src="assets/vendor/fancybox/js/jquery.fancybox.min.js"></script>
+	<script src="assets/vendor/owlcarousel/js/owl.carousel.min.js"></script>
+	<script src="assets/vendor/swiper/js/swiper.js"></script>
+	<script src="assets/vendor/wow/wow.min.js"></script>
 
 <!--Template Functions-->
 <script src="assets/js/functions.js"></script>
